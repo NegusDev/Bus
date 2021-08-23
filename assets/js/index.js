b473f1b1e9c2b4e4d6d4cca4c5d071a692dcd367
NegusDev <enochnegus@gmail.com>
2021-08-23 23:36:14 +0300

    krr

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
-if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("./sw.js").then(registration =>{
-		console.log("SW registered");
-		console.log(registration);
+// if ("serviceWorker" in navigator) {
+// 	navigator.serviceWorker.register("./sw.js").then(registration =>{
+// 		console.log("SW registered");
+// 		console.log(registration);
 		
-	}).catch(error =>{
-		console.log("Failed");
-		console.log(error);
-	});
-}
+// 	}).catch(error =>{
+// 		console.log("Failed");
+// 		console.log(error);
+// 	});
+// }
